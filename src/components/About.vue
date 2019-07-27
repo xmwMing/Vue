b45016bbaf7ef37f4faea6db465152ce63cb435d
xmwMing <48723775+xmwMing@users.noreply.github.com>
2019-07-27 10:04:35 +0800

    成员管理系统

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about container">
+    <p>联系方式</p>
+    <p>13265954342</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
